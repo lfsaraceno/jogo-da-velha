6262a017aa19172ed57cb1dc317b4aaac16d0a8f
Fernando <lfsaraceno@hotmail.com>
2020-03-19 16:01:05 -0300

    start tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> e salve para atualizar.
-        </p>
+    <div className="Sobre">
+      <header className="Sobre-header">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="Sobre-link"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Estude o React
+          Sobre
         </a>
       </header>
     </div>
