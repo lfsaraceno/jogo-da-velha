d834852fbc12bd2ba2658a2d73ee5df2044517c0
Fernando <lfsaraceno@hotmail.com>
2020-03-23 19:34:43 -0300

    Adicionado o Footer do App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Board from "./components/Board";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Board />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import Logo from "../../objects/Logo";
+
+const Footer = () => {
+  return (
+    <section className="footer">
+      <span className="copy">©</span>
+      <Logo />
+    </section>
+  );
+};
+
+export default Footer;
