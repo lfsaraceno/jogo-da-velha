8f1d31374379110fe3efb7043df53695e793b7ae
Fernando <lfsaraceno@hotmail.com>
2020-09-18 08:15:12 -0300

    feito transicao do sobre

--- src/objects/Close/index.js
+++ src/objects/Close/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ImageClose from "../../img/close.svg";
 
-const Close = () => {
-  return <img className="close" src={ImageClose} alt="" />;
-};
+const Close = () => (
+   <img className="close" src={ImageClose} alt="" />
+
+)
+
 
 export default Close;
